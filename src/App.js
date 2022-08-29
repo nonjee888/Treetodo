f3137aa56712b20285c0b99403893fc9b3c8b352
Yoepee <kdy2975@naver.com>
2022-08-29 15:02:45 +0900

    어떻게든 살려보자

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Router from "./shared/Router"
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Router/>
+    </>
+      );
+    }
+    export default App;
